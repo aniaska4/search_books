85f98032821f7e36c14a3c3fc0ba6cc58c0d970c
aniaska4 <aniaska4@wp.pl>
2020-02-19 15:21:23 +0100

    layout single book

--- src/components/SingleBook.vue
+++ src/components/SingleBook.vue
@@ -1,5 +1,25 @@
 <template lang="pug">
-    .name Ania
+<v-container>
+    <v-layout wrap>
+      <v-flex xs12 mr-1 ml-1>
+        <v-card>
+          <v-img></v-img>
+          <v-card-title primary-title>
+            <div>
+              <h2 class="headline mb-0"></h2>
+              <p> </p>
+              <h3>Actors:</h3>
+               <h4>Awards:</h4> 
+               <p>Genre:</p>
+            </div>
+          </v-card-title>
+          <v-card-actions>
+            <v-btn flat color="green" >back</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-layout>
+</v-container>
 </template>
 <script>
 export default {
